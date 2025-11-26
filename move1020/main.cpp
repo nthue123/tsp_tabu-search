116fa57623fd4eb742cbaa7e31e8d33338b76621
nthue123 <Hue.NT235734@sis.hust.edu.vn>
2025-11-26 11:12:28 +0700

    update

--- move1020/main.cpp
+++ move1020/main.cpp
@@ -140,7 +140,7 @@ void diversifyTour(vector<int>& tour, int strength) {
 }
 
 // ============================================================
-// 3. THUẬT TOÁN TABU SEARCH (FULL LOG VERSION)
+// 3. THUẬT TOÁN TABU SEARCH
 // ============================================================
 
 void runTabuSearch(string instanceName) {
@@ -150,7 +150,17 @@ void runTabuSearch(string instanceName) {
     const int TABU_TENURE = (N > 0) ? N/2 : 10;
     const int DIVERSIFY_THRESHOLD = 150;
 
-    cout << "--- TABU SEARCH RUNNING ---" << endl;
+    // --- MỞ FILE LOG ---
+    size_t lastdot = instanceName.find_last_of(".");
+    string rawName = (lastdot == string::npos) ? instanceName : instanceName.substr(0, lastdot);
+
+    // Tách tên file ra khỏi đường dẫn để đặt tên log
+    size_t lastSlash = rawName.find_last_of("/\\");
+    string pureName = (lastSlash == string::npos) ? rawName : rawName.substr(lastSlash + 1);
+
+    string logFilename = "TS_LOG_" + pureName + ".txt";
+    ofstream logFile(logFilename);
+    bool logEnabled = logFile.is_open();
 
     vector<int> currentTour = createNearestNeighborTour();
     double currentCost = calculateTourCost(currentTour);
@@ -163,15 +173,18 @@ void runTabuSearch(string instanceName) {
     tabuMatrix.assign(N, vector<int>(N, 0));
     int noImprovement = 0;
 
-    // --- IN TIÊU ĐỀ BẢNG ---
-    cout << "\n" << left
-         << setw(8) << "Iter"
-         << setw(15) << "Solution"
-         << setw(15) << "Object"
-         << setw(10) << "Next Best"
-         << setw(12) << "Move Type"
-         << setw(20) << "Move" << endl;
-    cout << string(80, '-') << endl;
+    // --- GHI TIÊU ĐỀ BẢNG VÀO FILE LOG ---
+    if (logEnabled) {
+        logFile << "--- TABU SEARCH LOG FOR INSTANCE: " << instanceName << " ---" << endl;
+        logFile << "\n" << left
+             << setw(8) << "Iter"
+             << setw(15) << "Current_Cost"
+             << setw(15) << "Best_Cost"
+             << setw(10) << "New_Best"
+             << setw(12) << "Move_Type"
+             << setw(20) << "Move" << endl;
+        logFile << string(80, '-') << endl;
+    }
 
     // --- Vòng lặp chính ---
     for (int k = 1; k <= MAX_ITERATIONS; ++k) {
@@ -196,30 +209,35 @@ void runTabuSearch(string instanceName) {
                     int B = currentTour[(i + 1) % N];
                     int X = currentTour[j];
                     int Y = currentTour[(j + 1) % N];
+
                     double rem = distMatrix[A][C] + distMatrix[C][B] + distMatrix[X][Y];
                     double add = distMatrix[A][B] + distMatrix[X][C] + distMatrix[C][Y];
                     delta = add - rem;
-                    if (tabuMatrix[A][B] > k || tabuMatrix[X][C] > k || tabuMatrix[C][Y] > k) isTabu = true;
+
+                    if (tabuMatrix[A][C] > k || tabuMatrix[C][B] > k) isTabu = true;
                 }
                 // Move 2-0
                 else if (moveType == 1) {
                     if (i >= N - 1) continue;
-                    int i_next = i + 1;
-                    if (j == i || j == i_next || j == (i - 1 + N) % N) continue;
+                    int i_next = (i + 1) % N;
+                    if (j == i || j == i_next || j == (i - 1 + N) % N || j == (i + 2) % N) continue;
                     int C = currentTour[i];
                     int D = currentTour[i_next];
                     int A = currentTour[(i - 1 + N) % N];
                     int B = currentTour[(i + 2) % N];
                     int X = currentTour[j];
                     int Y = currentTour[(j + 1) % N];
+
                     double rem = distMatrix[A][C] + distMatrix[D][B] + distMatrix[X][Y];
                     double add = distMatrix[A][B] + distMatrix[X][C] + distMatrix[D][Y];
                     delta = add - rem;
-                    if (tabuMatrix[A][B] > k || tabuMatrix[X][C] > k || tabuMatrix[D][Y] > k) isTabu = true;
+
+                    if (tabuMatrix[A][C] > k || tabuMatrix[D][B] > k) isTabu = true;
                 }
 
                 if (currentCost + delta > 0) {
                     bool aspiration = (currentCost + delta < bestCost);
+
                     if (delta < bestDelta) {
                         if (!isTabu || aspiration) {
                             bestDelta = delta;
@@ -232,13 +250,14 @@ void runTabuSearch(string instanceName) {
             }
         }
 
+        // --- THỰC HIỆN BƯỚC ĐI TỐT NHẤT VÀ CẬP NHẬT TRẠNG THÁI ---
         if (moveFound) {
             int A, B;
             string moveDescStr = "";
             string moveTypeStr = "";
             string nextBestStr = "No";
 
-            // Chuẩn bị thông tin log
+            // Cập nhật tour và Tabu List
             if (moveType == 0) {
                 moveTypeStr = "1-0 (Ins)";
                 int id_C = currentTour[best_i];
@@ -247,8 +266,10 @@ void runTabuSearch(string instanceName) {
 
                 A = currentTour[(best_i - 1 + N) % N];
                 B = currentTour[(best_i + 1) % N];
+
                 tabuMatrix[A][id_C] = tabuMatrix[id_C][A] = k + TABU_TENURE;
                 tabuMatrix[id_C][B] = tabuMatrix[B][id_C] = k + TABU_TENURE;
+
                 currentTour.erase(currentTour.begin() + best_i);
                 auto it = find(currentTour.begin(), currentTour.end(), id_X);
                 int new_pos = distance(currentTour.begin(), it);
@@ -257,15 +278,17 @@ void runTabuSearch(string instanceName) {
             else if (moveType == 1) {
                 moveTypeStr = "2-0 (Blk)";
                 int i = best_i;
-                int i_next = i + 1;
-                int j = best_j;
                 int id_C = currentTour[i];
-                int id_D = currentTour[i_next];
-                int id_X = currentTour[j];
+                int id_D = currentTour[i + 1];
+                int id_X = currentTour[best_j];
                 moveDescStr = "[" + to_string(id_C + 1) + "," + to_string(id_D + 1) + "]-" + to_string(id_X + 1);
 
                 A = currentTour[(i - 1 + N) % N];
+                int B = currentTour[(i + 2) % N];
+
                 tabuMatrix[A][id_C] = tabuMatrix[id_C][A] = k + TABU_TENURE;
+                tabuMatrix[id_D][B] = tabuMatrix[B][id_D] = k + TABU_TENURE;
+
                 currentTour.erase(currentTour.begin() + i, currentTour.begin() + i + 2);
                 auto it = find(currentTour.begin(), currentTour.end(), id_X);
                 int pos_X = distance(currentTour.begin(), it);
@@ -283,38 +306,45 @@ void runTabuSearch(string instanceName) {
                 nextBestStr = "Yes";
             }
 
-            // --- IN DÒNG LOG BẢNG ---
-            cout << left
-                 << setw(8) << k
-                 << setw(15) << fixed << setprecision(2) << currentCost
-                 << setw(15) << fixed << setprecision(2) << bestCost
-                 << setw(10) << nextBestStr
-                 << setw(12) << moveTypeStr
-                 << setw(20) << moveDescStr << endl;
+            // --- GHI DÒNG LOG BẢNG VÀ TABU LIST VÀO FILE LOG ---
+            if (logEnabled) {
+                logFile << left
+                        << setw(8) << k
+                        << setw(15) << fixed << setprecision(2) << currentCost
+                        << setw(15) << fixed << setprecision(2) << bestCost
+                        << setw(10) << nextBestStr
+                        << setw(12) << moveTypeStr
+                        << setw(20) << moveDescStr << endl;
 
-            // --- IN TABU LIST (CÁC CẠNH BỊ CẤM) ---
-            cout << "   |_ Tabu: ";
-            int countTabu = 0;
-            bool printed = false;
-            for(int r=0; r<min(N, 30); ++r) { // Chỉ check 30 thành phố đầu cho nhanh
-                for(int c=r+1; c<N; ++c) {
-                    if(tabuMatrix[r][c] > k) {
-                        cout << "(" << r+1 << "-" << c+1 << ":" << tabuMatrix[r][c] << ") ";
-                        countTabu++;
-                        printed = true;
-                        if(countTabu >= 8) break; // Chỉ in tối đa 8 cạnh để không rối
+                // Ghi Tabu List
+                logFile << "    |_ Tabu: ";
+                int countTabu = 0;
+                bool printed = false;
+                for(int r=0; r<N; ++r) {
+                    for(int c=r+1; c<N; ++c) {
+                        if(tabuMatrix[r][c] > k) {
+                            logFile << "(" << r+1 << "-" << c+1 << ":" << tabuMatrix[r][c] << ") ";
+                            countTabu++;
+                            printed = true;
+                            if(countTabu >= 8) goto end_tabu_loop;
+                        }
                     }
                 }
+                end_tabu_loop:;
+                if(countTabu >= 8) logFile << "...";
+                if(!printed) logFile << "EMPTY";
+                logFile << endl;
             }
-            if(countTabu >= 8) cout << "...";
-            if(!printed) cout << "EMPTY";
-            cout << endl; // Xuống dòng
         }
 
+        // --- DIVERSIFICATION ---
         if (noImprovement >= DIVERSIFY_THRESHOLD) {
-            cout << string(80, '-') << endl;
-            cout << "!!! DIVERSIFYING (SHUFFLE) !!!" << endl;
-            cout << string(80, '-') << endl;
+            if (logEnabled) {
+                logFile << string(80, '-') << endl;
+                logFile << "!!! DIVERSIFYING (SHUFFLE) at Iteration " << k << " !!!" << endl;
+                logFile << "Current Cost before Diversification: " << fixed << setprecision(2) << currentCost << endl;
+                logFile << string(80, '-') << endl;
+            }
             diversifyTour(currentTour, N);
             currentCost = calculateTourCost(currentTour);
             tabuMatrix.assign(N, vector<int>(N, 0));
@@ -322,16 +352,23 @@ void runTabuSearch(string instanceName) {
         }
     }
 
+    // --- ĐÓNG FILE LOG ---
+    if (logEnabled) {
+        logFile.close();
+    }
+
     auto end_time = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = end_time - start_time;
 
+    // --- BẮT ĐẦU VÙNG REPORT (Dùng để trích xuất) ---
+    cout << "!!!REPORT_START!!!" << endl;
+
+    // --- FINAL RESULT REPORT (In ra màn hình console) ---
     cout << "\n========================================" << endl;
-    cout << "           FINAL RESULT REPORT          " << endl;
+    cout << "          FINAL RESULT REPORT           " << endl;
     cout << "========================================" << endl;
 
-    size_t lastdot = instanceName.find_last_of(".");
-    string rawName = (lastdot == string::npos) ? instanceName : instanceName.substr(0, lastdot);
-    cout << "Instance    : " << rawName << endl;
+    cout << "Instance    : " << pureName << endl;
     cout << "Runtime     : " << fixed << setprecision(4) << elapsed.count() << " s" << endl;
     cout << "Best Iter   : " << bestIter << endl;
     cout << "Tabu Tenure : " << TABU_TENURE << endl;
@@ -343,15 +380,35 @@ void runTabuSearch(string instanceName) {
     }
     cout << "]" << endl;
     cout << "========================================" << endl;
+
+    // --- KẾT THÚC VÙNG REPORT (Dùng để trích xuất) ---
+    cout << "!!!REPORT_END!!!" << endl;
+
+    if (logEnabled) {
+        // Thông báo cho người dùng biết file log đã được tạo (in sau REPORT_END)
+        cout << "\n✅ Log chi tiết đã được lưu vào file: " << logFilename << endl;
+    }
 }
 
 // ============================================================
-// 4. HÀM MAIN
+// 4. HÀM MAIN (Đã sửa để nhận đối số dòng lệnh)
 // ============================================================
-int main() {
+int main(int argc, char* argv[]) {
     srand(unsigned(time(0)));
-    string filename = "eil51.tsp";
+
+    // KIỂM TRA ĐỐI SỐ DÒNG LỆNH
+    if (argc != 2) {
+        cerr << "LỖI SỬ DỤNG: Cú pháp: ./tsp_solver <tên_file_tsp>" << endl;
+        cerr << "Ví dụ: ./tsp_solver data/eil51.tsp" << endl;
+        return 1;
+    }
+
+    // Lấy tên file từ đối số dòng lệnh đầu tiên (argv[1])
+    string filename = argv[1];
+
     readInput(filename);
+
     if (N > 0) runTabuSearch(filename);
+
     return 0;
 }
